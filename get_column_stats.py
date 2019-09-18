76cc11e841e12c8f8781bd3a483011d04fd4d26a
humzaashraf <40871986+humzaashraf@users.noreply.github.com>
2019-09-18 16:56:55 -0600

    Update get_column_stats.py

--- get_column_stats.py
+++ get_column_stats.py
@@ -2,32 +2,90 @@ import sys
 import math
 import argparse
 
-parser = argparse.ArgumentParser(
-            description='input file and column number; returns the mean & std',
-            prog='input arg')
 
-parser.add_argument('file_name', type=str, help='Name of the file')
+def col_from_file(file_name, col_num): 
+    """Take a file name and a column number and create an array
 
-parser.add_argument('col_num', type=int, help='The column number')
+    Parameters
+    ----------
+    
+    file_name : string
+    col_num : integer
 
-args = parser.parse_args()
+    Returns
+    -------
+    V : fill empty array w/ integers from specified column 
 
-print(args.file_name, args.col_num)
+    """
+	V = []
 
-# file_name = sys.argv[1]
-# col_num = int(sys.argv[2])
+	f = open(file_name, 'r')
 
-f = open(args.file_name, 'r')
+	for l in f:
+	    A = [int(x) for x in l.split()]
+	    V.append(A[col_num])
 
-V = []
+	    return(V)
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[args.col_num])
 
-mean = sum(V)/len(V)
+def mean_calc(data_list): 
+    """ Take the mean and standard deviation of an array
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    Arguments
+    ---------
+    data_list : array of integers
 
-print('mean:', mean)
-print('stdev:', stdev)
+    Returns
+    -------
+    mean : mean
+    stdev : standard deviation
+
+    """
+	mean = sum(data_list)/len(data_list)
+	stdev = math.sqrt(sum([(mean-x)**2 for x in data_list]) / len(data_list))
+
+	return(mean)
+	return(stdev)
+
+
+def main():
+	parser = argparse.ArgumentParser(
+	         description='input file and column number; returns the mean & std',
+	         prog='input arg')
+
+	parser.add_argument('file_name', type=str, help='Name of the file')
+	parser.add_argument('col_num', type=int, help='The column number')
+	
+	args = parser.parse_args()
+	
+	print(args.file_name, args.col_num)
+
+	# Functional validation of output with various argument errors
+	
+	try:
+		int(args.col_num) = None 
+	except ValueError
+		print('Column index must be an integer')
+		sys.exit(1)
+
+	try:
+		open(file_name, 'r') = None
+	except FileNotFoundError
+		print('No file found')
+		sys.exit(1)
+
+	try:
+		col_from_file(args.file_name,args.col_num) = None 
+	except IndexError
+		print('File or list index out of range')
+		sys.exit(1)
+
+	try:
+		str(args.file_name) = None
+	except PermissionError
+		print('File cannot be accessed')
+		sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
