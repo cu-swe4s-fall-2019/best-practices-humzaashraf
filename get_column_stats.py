e88608820dd9f53b82fc2ce253cec7771e61828f
humzaashraf <40871986+humzaashraf@users.noreply.github.com>
2019-09-19 10:34:51 -0600

    Update get_column_stats.py

--- get_column_stats.py
+++ get_column_stats.py
@@ -17,15 +17,15 @@ def col_from_file(file_name, col_num):
     V : fill empty array w/ integers from specified column 
 
     """
-	V = []
+    V = []
 
-	f = open(file_name, 'r')
+    f = open(file_name, 'r')
 
-	for l in f:
-	    A = [int(x) for x in l.split()]
-	    V.append(A[col_num])
+    for l in f:
+        A = [int(x) for x in l.split()]
+        V.append(A[col_num])
 
-	    return(V)
+    return(V)
 
 
 def mean_calc(data_list): 
@@ -41,50 +41,50 @@ def mean_calc(data_list):
     stdev : standard deviation
 
     """
-	mean = sum(data_list)/len(data_list)
-	stdev = math.sqrt(sum([(mean-x)**2 for x in data_list]) / len(data_list))
+    mean = sum(data_list)/len(data_list)
+    stdev = math.sqrt(sum([(mean-x)**2 for x in data_list]) / len(data_list))
 
-	return(mean)
-	return(stdev)
+    return(mean)
+    return(stdev)
 
 
 def main():
-	parser = argparse.ArgumentParser(
-	         description='input file and column number; returns the mean & std',
-	         prog='input arg')
+    parser = argparse.ArgumentParser(
+             description='input file and column number; returns the mean & std',
+             prog='input arg')
 
-	parser.add_argument('file_name', type=str, help='Name of the file')
-	parser.add_argument('col_num', type=int, help='The column number')
-	
-	args = parser.parse_args()
-	
+    parser.add_argument('file_name', type=str, help='Name of the file')
+    parser.add_argument('col_num', type=int, help='The column number')
+
+    args = parser.parse_args()
+
 	print(args.file_name, args.col_num)
 
-	# Functional validation of output with various argument errors
-	
-	try:
-		int(args.col_num) == None 
-	except ValueError:
-		print('Column index must be an integer')
-		sys.exit(1)
+    # Functional validation of output with various argument errors
 
-	try:
-		open(file_name, 'r') == None
-	except FileNotFoundError:
-		print('No file found')
-		sys.exit(1)
+    try:
+        int(args.col_num) == None 
+    except ValueError:
+        print('Column index must be an integer')
+        sys.exit(1)
 
-	try:
-		col_from_file(args.file_name,args.col_num) == None 
-	except IndexError:
-		print('File or list index out of range')
-		sys.exit(1)
+    try:
+        open(file_name, 'r') == None
+    except FileNotFoundError:
+        print('No file found')
+        sys.exit(1)
 
-	try:
-		str(args.file_name) == None
-	except PermissionError:
-		print('File cannot be accessed')
-		sys.exit(1)
+    try:
+        col_from_file(args.file_name,args.col_num) == None 
+    except IndexError:
+        print('File or list index out of range')
+        sys.exit(1)
+
+    try:
+        str(args.file_name) == None
+    except PermissionError:
+        print('File cannot be accessed')
+        sys.exit(1)
 
 
 if __name__ == '__main__':
