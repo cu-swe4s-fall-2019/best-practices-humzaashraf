f2b49f4ae81d07652800f5b51bc20d29eff48020
humzaashraf <40871986+humzaashraf@users.noreply.github.com>
2019-09-19 18:19:36 -0600

    Update basics_test.py

--- basics_test.py
+++ basics_test.py
@@ -1,6 +1,37 @@
+import sys
+import os
+import random
 import unittest
 import get_column_stats
 
 class TestCalc(unittest.TestCase):
-	def test_mean(self):
-		self.Is(calc.mean_calc())
+
+    def setUp(self):
+
+        self.direct_sum = 0
+        self.vector = []
+
+        for i in range(100):
+            rand_int = random.randint(1,100)
+            self.direct_sum += rand_int
+            self.vector.append(rand_int)  
+
+        #rand_int_numpy = np.random.randint(100)
+        #a = np.random.choice([1, 0], a.shape, p=[.1, .9]).astype(bool)
+        #a[mask] = np.nan
+
+        #for i in range(100):
+        #rand_int_nan = random
+
+        self.mean = self.direct_sum/100
+
+
+    def test_mean(self):
+        print(self.direct_sum)
+        print(self.mean)
+        var = get_column_stats.mean_calc(self.vector)
+        self.assertEqual(var,self.mean)
+
+
+if __name__ == '__main__':
+    unittest.main()
